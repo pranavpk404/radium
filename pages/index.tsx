15b7d62bb9f76e56062bc2e6802d4c6fc8df9d41
Pranav <pranavpk404@gmail.com>
2022-05-16 17:26:40 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Radium search the web</title>
+        <meta name="description" content="Radium search the web" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="">Hello</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
